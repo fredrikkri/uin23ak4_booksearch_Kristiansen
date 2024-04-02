448b76484590d1c7f1cee5b797c079870e9a693b
fredrikkri <fjkri@hotmail.com>
2024-04-02 15:25:59 +0200

    viser "none" på ratings hvis det ikke finnes ogcss endringer

--- oblig4/src/components/Bookcard.jsx
+++ oblig4/src/components/Bookcard.jsx
@@ -1,13 +1,14 @@
-export default function Bookcard({keyBook, title, first_publish_year, author_name, ratings_average, coverImage, id_amazon}) {
+export default function Bookcard({keyBook, title, first_publish_year, author_name, ratings_average, coverImage, isbn}) {
 
+    console.log("isbn",isbn?.length)
     return (
     <>
         <article className="card" key={keyBook}>
         <h3>{title}</h3>
         <p>First publish year: {first_publish_year}</p>
         <p>Author: {author_name}</p>
-        <p>Average rating: {ratings_average}</p>
-        <a href={id_amazon}>Go to Amazon</a>
+        {ratings_average == null ? <p>Average rating: none</p> : <p>Average rating: {ratings_average}</p>} 
+        {isbn == null ? "" : <a href={`https://www.amazon.com/s?k=${isbn?.[0]}`}>Show in Amazon</a>}
         <img src={`https://covers.openlibrary.org/b/id/${coverImage}-L.jpg`} alt={title}/>
         {/* For å få tilgang til bilder for hver bok så fulgte jeg API-dokumentasjon på openlibrary.org */}
         {/* https://openlibrary.org/dev/docs/api/covers */}
--- oblig4/src/components/SearchResults.jsx
+++ oblig4/src/components/SearchResults.jsx
@@ -8,13 +8,13 @@ export default function SearchResults({content}){
     <span className="display-Books">
         {allBooks?.map(singleBook => 
         <Bookcard className="card" 
-            key={singleBook._version_} 
-            title={singleBook.title} 
-            first_publish_year={singleBook.first_publish_year}
-            author_name={singleBook.author_name}
-            ratings_average={singleBook.ratings_average}
-            coverImage={singleBook.cover_i}
-            id_amazon={singleBook.id_amazon}>
+            key={singleBook?._version_} 
+            title={singleBook?.title} 
+            first_publish_year={singleBook?.first_publish_year}
+            author_name={singleBook?.author_name}
+            ratings_average={singleBook?.ratings_average}
+            coverImage={singleBook?.cover_i}
+            isbn={singleBook?.isbn}>
         </Bookcard>)}
     </span>
     )
